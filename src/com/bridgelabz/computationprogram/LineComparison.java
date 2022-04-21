b4fdf103052a14db0b732c638e1283ba852e2056
namo-patil <namratapatil74738@gmail.com>
2022-04-21 09:37:09 +0530

    Added: Program to calculate length of line using cartesian system

--- src/com/bridgelabz/computationprogram/LineComparison.java
+++ src/com/bridgelabz/computationprogram/LineComparison.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.computationprogram;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of co-ordinates x1, y1, x2, y2 :");
+        double x1 = scanner.nextInt(), y1 = scanner.nextInt(), x2 = scanner.nextInt(), y2 = scanner.nextInt();
+        double length_Line = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Using Cartesian System");
+        System.out.println("Length of Line (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") is :" + length_Line);
     }
 }
