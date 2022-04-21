010dab37e3e79378cdee8fa390256469ec907385
namo-patil <namratapatil74738@gmail.com>
2022-04-21 14:53:58 +0530

    Modified: Program to check equality of two lines using equals method

--- src/com/bridgelabz/computationprogram/LineComparison.java
+++ src/com/bridgelabz/computationprogram/LineComparison.java
@@ -7,25 +7,22 @@ public class LineComparison {
         System.out.println("Welcome to Line Comparison Computation Program");
         checkEqualityOfTwoLines();
     }
-    public String calLengthOfLine() {
+
+    static String calLengthOfLine() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the value of co-ordinates x1, y1, x2, y2 :");
         int x1 = scanner.nextInt(), y1 = scanner.nextInt(), x2 = scanner.nextInt(), y2 = scanner.nextInt();
-        String length_Line = String.valueOf(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-        System.out.println("Using Cartesian System");
-        System.out.println("Length of Line (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") is :" + length_Line);
+        String lengthOfLine = String.valueOf(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+        System.out.println("Length of Line (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") is :" + lengthOfLine);
         System.out.println();
-        return length_Line;
+        return lengthOfLine;
     }
-    static void checkEqualityOfTwoLines(){
-        LineComparison lineComparison = new LineComparison();
-        String line_1 = lineComparison.calLengthOfLine();
-        String line_2 = lineComparison.calLengthOfLine();
-        System.out.println("Using Equals Method");
-        if (line_1.equals(line_2)) {
-            System.out.println("Lines are equal");
-        } else {
-            System.out.println("Lines are Distinct");
-        }
+
+    // Prints true => equals, and Prints false => distinct
+    static void checkEqualityOfTwoLines() {
+        String line1 = calLengthOfLine();
+        String line2 = calLengthOfLine();
+        System.out.println("---Equals Method---");
+        System.out.println(line1.equals(line2));
     }
 }
